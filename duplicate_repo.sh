502f9db67890e6255ebbc434c0f6e125f7354c2d
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-01-17 22:35:15 +0100

    Removed code in comment, duplicate to multiple repositories

--- duplicate_repo.sh
+++ duplicate_repo.sh
@@ -7,38 +7,21 @@
 set -o errexit # abort on nonzero exitstatus
 set -o nounset # abort on unbound variable
 
-#{{{ Functions
-
-#usage() {
-#cat << _EOF_
-#Usage: ${0} 
-
-#_EOF_
-#}
-
-##}}}
-##{{{ Command line parsing
-
-#if [ "$#" -ne "1" ]; then
-    #echo "Expected 1 argument, got $#" >&2
-    #usage
-    #exit 2
-#fi
-
-#}}}
 #{{{ Variables
 org=HoGentTIProjecten1
 source_repo=testrepo
-dest_repo=p1g20
+dest_repo_prefix=p1g
+from=20
+to=20
 #}}}
 
 # Script proper
-git clone --bare "git@github.com:${org}/${source_repo}.git"
-
+# Source: https://help.github.com/articles/duplicating-a-repository/
+echo git clone --bare "git@github.com:${org}/${source_repo}.git"
 cd "${source_repo}.git"
 
-git push --mirror "git@github.com:${org}/${dest_repo}.git"
-
+for i in $(seq -f "%02g" ${from} ${to}); do
+  echo git push --mirror "git@github.com:${org}/${dest_repo_prefix}${i}.git"
+done
 cd ..
-
 rm -rf "${source_repo}.git"
