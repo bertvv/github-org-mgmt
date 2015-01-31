8c2fbdb39ff63bc766942377ec74985b3ccfdd8f
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-01-31 20:03:31 +0100

    Added script to make exact copies of a repo

---
+++ mirror_repos.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/bash
+#
+# Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# 
+
+set -o errexit # abort on nonzero exitstatus
+set -o nounset # abort on unbound variable
+
+#{{{ Functions
+
+usage() {
+cat << _EOF_
+Usage: ${0} 
+
+_EOF_
+}
+
+#}}}
+#{{{ Variables
+repo_regex='[ag]_'
+repos=$(./list_repos.py | grep "${repo_regex}")
+local_dir="${HOME}/prj_repos"
+#}}}
+
+# Script proper
+
+if [ ! -d "${local_dir}" ]; then
+  mkdir -p "${local_dir}"
+fi
+
+cd "${local_dir}"
+
+for repo in ${repos}; do
+  git clone --bare "git@github.com:HoGentTIN/${repo}.git"
+done
+
