c2a0b76398cf1b6da9ee227dd4a60b7f499c6ffe
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-09-23 20:22:01 +0200

    add command line option and help message

--- mirror_repos.sh
+++ mirror_repos.sh
@@ -11,19 +11,33 @@ set -o nounset # abort on unbound variable
 
 usage() {
 cat << _EOF_
-Usage: ${0} 
-
+Usage: ${0} PATTERN
+  Makes a bare clone of all repositories in the organization matching
+  PATTERN.
 _EOF_
 }
 
 #}}}
+#{{{ Command line parsing
+
+if [ "$#" -ne "1" ]; then
+    echo "Expected 1 argument, got $#" >&2
+    usage
+    exit 2
+fi
+
+#}}}
 #{{{ Variables
-repo_regex='[ag]_'
+repo_regex=$1
 repos=$(./list_repos.py | grep "${repo_regex}")
 local_dir="${HOME}/prj_repos"
 #}}}
 
 # Script proper
+
+if [ -z "${repos}" ]; then
+  echo "No repos found that match the search string"
+fi
 
 if [ ! -d "${local_dir}" ]; then
   mkdir -p "${local_dir}"
