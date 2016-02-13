cb0454327e039360663b46d344741fb14300c4a3
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-02-13 15:50:56 +0100

    Added command line script

---
+++ gom.py
@@ -0,0 +1,94 @@
+#! /usr/bin/python
+# coding=utf8
+# gom.py -- Command line tool for managing a Github organization
+
+import sys
+from github_org import GithubOrganizationManager as OrgMgr
+
+#
+# Helper functions
+#
+
+
+def usage():
+    print"""
+Usage %s ORGANIZATION ACTION [OPTION]...
+
+ORGANIZATION is the name of the Github organization to be managed. You should
+  have a configuration file named 'ORGANIZATION-conf.yml' in the working
+  directory.
+
+ACTIONS
+
+  c, create-teams CSV     creates teams and members from the specified CSV file
+  d, delete-teams PREFIX  delete all teams and associated repos that have a
+                          name starting with PREFIX
+  l, list-repos           prints all repositories in the organization
+
+"""[1:-1] % sys.argv[0]
+
+
+def list_repos(manager):
+    """List repositories in the organization."""
+    repos = manager._organization.get_repos()
+    for repo in repos:
+        print repo.name
+
+
+def create_teams(manager, options):
+    """Create new teams and repositories"""
+    if len(options) < 1:
+        print "No user file specified!"
+        usage()
+        sys.exit(1)
+
+    user_file = options[0]
+
+    print "Fetching users and teams from %s. This may take a while." \
+        % user_file
+    print "Failed users (if any):"
+    teams = manager.read_teams_from_csv(user_file)
+
+    print "Adding teams to organization"
+    manager.add_teams_to_org(teams)
+
+
+def delete_teams(manager, options):
+    """Delete teams and their repos starting with the specified prefix"""
+    if len(options) < 1:
+        print "No name prefix of teams to delete specified!"
+        usage()
+        sys.exit(1)
+
+    prefix = options[0]
+
+    manager.delete_teams_and_repos(prefix)
+
+#
+# Script proper
+#
+
+if len(sys.argv) < 3:
+    print "Not enough arguments, expected at least 2"
+    usage()
+    sys.exit(2)
+
+organization_name = sys.argv[1]
+action = sys.argv[2]
+options = sys.argv[3:]
+manager = OrgMgr(organization_name)
+
+# print "Org    : %s" % manager._organization_name
+# print "Action : %s" % action
+# print "Options: %s" % ', '.join(options)
+
+if action == "list-repos" or action == "l":
+    list_repos(manager)
+elif action == "create-teams" or action == "c":
+    create_teams(manager, options)
+elif action == "delete-teams" or action == "d":
+    delete_teams(manager, options)
+else:
+    print "Unknown action: %s" % action
+    usage()
+    sys.exit(1)
