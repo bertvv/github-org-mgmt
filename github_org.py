e34f845e3cab551c8bad40b1ee439a833d4534bd
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-02-13 15:48:44 +0100

    Config in Yaml instead of JSON. Improved team/repo deletion

--- github_org.py
+++ github_org.py
@@ -1,7 +1,7 @@
 # coding=utf8
 #
 
-import json
+import yaml
 import csv
 from github import Github, NamedUser
 from github import GithubException
@@ -11,11 +11,18 @@ class GithubOrganizationManager:
     __slots__ = []
     MIN_PREFIX_LENGTH = 3
 
-    def __init__(self, config_file_name):
-        self._config = self.read_config(config_file_name)
+    def __init__(self, organization_name):
+        self._organization_name = organization_name
+        self._config = self.read_config()
         self._github = Github(self._config['user'], self._config['password'])
         self._organization = \
             self._github.get_organization(self._config['organization'])
+
+    def repo_names(self):
+        names = self._config['repos']
+        if len(names) == 0:
+            names.append('')
+        return names
 
     def read_teams_from_csv(self, csvfile):
         """Read all teams and their users from the specified CSV file.
@@ -57,18 +64,19 @@ class GithubOrganizationManager:
 
         return teams
 
-    def read_config(self, config_file_name='ghorg.conf'):
-        """Read the configuration from the specified file.
-        The file should be in JSON format. See ‘ghorg.conf.example’.
+    def read_config(self):
+        """Read the configuration for this organization.
+        The file should be in Yaml format. See ‘org-conf.yml.example’.
         """
+        config_file_name = self._organization_name + "-conf.yml"
         try:
             with open(config_file_name) as config_file:
-                config = json.load(config_file)
+                config = yaml.load(config_file)
                 return config
         except IOError:
             print ("Couldn't load configuration file ‘%s’. "
                    "Maybe you should create it?\n"
-                   "See config.example for an example") % config_file_name
+                   "See e.g. org-conf.yml.example") % config_file_name
             raise SystemExit
 
     def add_teams_to_org(self, teams):
@@ -77,24 +85,24 @@ class GithubOrganizationManager:
         repo_config = self._config['repo_config']
 
         for team_name in teams.keys():
-            print "^_^ %s ^_^" % team_name
+            print "- team: %s\n  repos:" % team_name
             team = self._organization.create_team(team_name)
             team.edit(team_name, permission=self._config['repo_access'])
 
-            # Create 3 repos for each team:
-            for rname in ['Java', 'JavaWeb', 'DotNet']:
+            for rname in self.repo_names():
                 repo_name = team_name + rname
-                print "    Repo: %s" % repo_name
+                print "    - %s" % repo_name
                 repo = self._organization.create_repo(repo_name, **repo_config)
                 team.add_to_repos(repo)
 
+            print "  users:"
             self.add_members_to_team(team, teams[team_name])
 
     def add_members_to_team(self, team, members):
         """Adds the members, a list of NamedUsers, to the team"""
 
         for member in members:
-            print "    %s" % member.login
+            print "    - %s" % member.login
             self.__pygithub_add_membership(team, member)
 
     def __pygithub_add_membership(self, team, member):
@@ -109,91 +117,6 @@ class GithubOrganizationManager:
             "PUT", team.url + "/memberships/" + member._identity
         )
 
-    def delete_teams_from_org(self, prefix):
-        """Delete all teams whose name starts with the specified prefix from
-        the organization. This also deletes any repository with the same name
-        as the team, if it exists. THIS CANNOT BE UNDONE!"""
-
-        assert len(prefix) >= self.MIN_PREFIX_LENGTH, \
-            ("Team name prefix is very short. This implies that a lot of teams"
-             "may be deleted. Please use a prefix of at least %s characters") \
-            % self.MIN_PREFIX_LENGTH
-
-        teams_to_delete = self.get_teams_starting_with(prefix)
-
-        if len(teams_to_delete) == 0:
-            print "No teams start with %s, bailing out" % prefix
-            return
-
-        print '=' * 80
-        print '!!! WARNING WARNING WARNING !!!'
-        print "This deletes all teams starting with prefix %s" % prefix
-        print 'and their repositories from the organization.'
-        print '!!! THIS CANNOT BE UNDONE !!!'
-        print '=' * 80
-        print 'Teams to be deleted:'
-        print ', '.join([team.name for team in teams_to_delete])
-        print '=' * 80
-        print 'Type in the prefix again to confirm: '
-
-        choice = raw_input().lower()
-
-        if choice != prefix:
-            print 'Confirmation failed, bailing out.'
-            return
-
-        for team in teams_to_delete:
-                print "Deleting %s" % team.name
-                self.delete_team_repo_from_org(team.name)
-                team.delete()
-
-    def delete_team_repo_from_org(self, repo_name):
-        """Delete the repository with the specified name from the organization.
-        If the repository does not exist, a warning is printed"""
-
-        try:
-            repo = self._organization.get_repo(repo_name)
-            repo.delete()
-        except:
-            print u"    Repo ‘%s’ already gone. Ignoring..." % repo_name
-
-    def delete_repos_from_org(self, prefix):
-        """Delete all teams whose name starts with the specified prefix from
-        the organization. This also deletes any repository with the same name
-        as the team, if it exists. THIS CANNOT BE UNDONE!"""
-
-        assert len(prefix) >= self.MIN_PREFIX_LENGTH, \
-            ("Team name prefix is very short. This implies that a lot of teams"
-             "may be deleted. Please use a prefix of at least %s characters") \
-            % self.MIN_PREFIX_LENGTH
-
-        repos_to_delete = self.get_repos_starting_with(prefix)
-
-        if len(repos_to_delete) == 0:
-            print "No repos start with %s, bailing out" % prefix
-            return
-
-        print '=' * 80
-        print '!!! WARNING WARNING WARNING !!!'
-        print "This deletes all repos starting with prefix %s" % prefix
-        print 'the organization.'
-        print '!!! THIS CANNOT BE UNDONE !!!'
-        print '=' * 80
-        print 'Repos to be deleted:'
-        print ', '.join([repo.name for repo in repos_to_delete])
-        print '=' * 80
-        print 'Type in the prefix again to confirm: '
-
-        choice = raw_input().lower()
-
-        if choice != prefix:
-            print 'Confirmation failed, bailing out.'
-            return
-
-        for repo in repos_to_delete:
-            print "Deleting %s" % repo.name
-            repo.delete()
-
     def get_teams_starting_with(self, prefix):
         teams = []
         for team in self._organization.get_teams():
@@ -207,3 +130,67 @@ class GithubOrganizationManager:
             if repo.name.startswith(prefix):
                 repos.append(repo)
         return repos
+
+    def delete_repo(self, repo_name):
+        """Delete the repository with the specified name from the organization.
+        If the repository does not exist, a warning is printed"""
+
+        try:
+            repo = self._organization.get_repo(repo_name)
+            repo.delete()
+        except:
+            print u"    Repo ‘%s’ already gone. Ignoring..." % repo_name
+
+    def delete_repos(self, repos_to_delete):
+        """Delete specified repos. THIS CANNOT BE UNDONE!"""
+
+        for repo in repos_to_delete:
+            print "Deleting %s" % repo.name
+            repo.delete()
+
+    def delete_teams(self, teams_to_delete):
+        """Delete specified teams. THIS CANNOT BE UNDONE!"""
+
+        for team in teams_to_delete:
+                print "Deleting %s" % team.name
+                team.delete()
+
+    def delete_teams_and_repos(self, prefix):
+        """Delete all teams whose name starts with the specified prefix from
+        the organization. This also deletes any repository starting with the
+        team name, if it exists. THIS CANNOT BE UNDONE!"""
+
+        assert len(prefix) >= self.MIN_PREFIX_LENGTH, \
+            ("Team name prefix is too short. may be deleted. Please use a "
+             "prefix of at least %s characters") \
+            % self.MIN_PREFIX_LENGTH
+
+        teams_to_delete = self.get_teams_starting_with(prefix)
+        repos_to_delete = self.get_repos_starting_with(prefix)
+
+        if len(teams_to_delete) == 0 and len(repos_to_delete) == 0:
+            print "No teams or repos start with %s, bailing out" % prefix
+            return
+
+        print '=' * 80
+        print '!!! WARNING WARNING WARNING !!!'
+        print "This deletes all teams starting with prefix %s" % prefix
+        print 'and their repositories from the organization.'
+        print '!!! THIS CANNOT BE UNDONE !!!'
+        print '=' * 80
+        print 'Teams to be deleted:'
+        print ', '.join([team.name for team in teams_to_delete])
+        print '-' * 80
+        print 'Repos to be deleted:'
+        print ', '.join([repo.name for repo in repos_to_delete])
+        print '=' * 80
+        print 'Type in the prefix again to confirm: '
+
+        choice = raw_input().lower()
+
+        if choice != prefix:
+            print 'Confirmation failed, bailing out.'
+            return
+
+        self.delete_teams(teams_to_delete)
+        self.delete_repos(repos_to_delete)
